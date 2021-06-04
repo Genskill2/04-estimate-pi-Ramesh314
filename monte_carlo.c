6a664b88398b2c26af6073feb7326ffea7162c0e
Ramesh314 <84651490+Ramesh314@users.noreply.github.com>
2021-06-04 14:01:35 +0530

    Update

--- monte_carlo.c
+++ monte_carlo.c
@@ -1,22 +1,31 @@
-
-
-
-
 #include <assert.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
-#define random rand
-#define srandom srand
-
-
-float mc_pi(int);
-
 float frandom() {
   long int q = random();
   float ret = (float)q/(float)RAND_MAX;
   return ret;
 }
+
+float mc_pi(int n)
+{	
+	int pc=0,ps=n;
+	
+	for(int i=0; i<n; i++){
+	float x,y,len;
+	x=frandom();
+	y=frandom();
+	len =x*x+y*y;
+	if(len<=1){
+	pc++;
+	}
+	}
+	float pi = 4.0*pc/ps;
+	return pi;
+}
+
+
 
 int main(void) {
   float pi0;
@@ -44,23 +53,4 @@ int main(void) {
       abort();
     }
   }
-}
-
-float mc_pi(int n)
-{  float pi,x,y,z;
-int count=0;
-    for(int i = 0; i < n; ++i) {
-
-     x = frandom();
-
-     y = frandom();
-
-     z = x * x + y * y;
-
-     if( z <= 1 ) count++;
- }
-
- pi = (double) count / n * 4;
-
- return pi;
 }
